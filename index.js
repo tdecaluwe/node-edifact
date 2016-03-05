7589b3e5f95a5064f471d67cd691f595b7d33fb1
Tom De Caluwé <decaluwe.t@gmail.com>
2016-03-05 07:15:03 +0100

    Improved Tracker implementation.

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 module.exports.Counter = require('./counter.js');
 module.exports.Validator = require('./validator.js');
 module.exports.Parser = require('./parser.js');
+module.exports.Tracker = require('./tracker.js');
 module.exports.Reader = require('./reader.js');
--- spec/TrackerSpec.js
+++ spec/TrackerSpec.js
@@ -4,21 +4,22 @@ import Tracker from "../tracker.js";
 
 describe('Tracker', function () {
   let tracker;
-  describe('expecting a mandatory segment', function () {
-    beforeEach(function () {
-      tracker = new Tracker([
-        { content: 'AAA', mandatory: true, repetition: 1 },
-        { content: 'BBB', mandatory: false, repetition: 1 }
-      ]);
-    });
-    it('shouldn\'t accept unknown segments', function () {
-      expect(function () { tracker.accept('SEG'); }).toThrow();
-    });
-    it('should throw if omitted', function () {
-      expect(function () { tracker.accept('BBB'); }).toThrow();
-    });
+  it('shouldn\'t accept unknown segments', function () {
+    tracker = new Tracker([
+      { content: 'AAA', mandatory: true, repetition: 1 }
+    ]);
+    expect(function () { tracker.accept('SEG'); }).toThrow();
+  });
+  it('should throw if omitting a mandatory segment', function () {
+    tracker = new Tracker([
+      { content: 'AAA', mandatory: true, repetition: 1 },
+      { content: 'BBB', mandatory: false, repetition: 1 }
+    ]);
+    expect(function () { tracker.accept('BBB'); }).toThrow();
   });
   describe('expecting a mandatory repeatable segment', function () {
+    // Such a segment defines a repetition property higher than one. However
+    // a mandatory segment only requires the segment to be included once.
     beforeEach(function () {
       tracker = new Tracker([
         { content: 'AAA', mandatory: true, repetition: 2 },
@@ -41,9 +42,6 @@ describe('Tracker', function () {
         { content: 'AAA', mandatory: false, repetition: 2 },
         { content: 'BBB', mandatory: false, repetition: 1 }
       ]);
-    });
-    it('shouldn\'t accept unknown segments', function () {
-      expect(function () { tracker.accept('SEG'); }).toThrow();
     });
     it('can skip to the next entry', function () {
       expect(function () { tracker.accept('BBB'); }).not.toThrow();
@@ -84,25 +82,27 @@ describe('Tracker', function () {
       tracker = new Tracker([
         { content: [
           { content: 'AAA', mandatory: true, repetition: 1 },
-          { content: 'BBB', mandatory: false, repetition: 1 }
+          { content: 'BBB', mandatory: false, repetition: 1 },
         ], mandatory: false, repetition: 3},
-        { content: 'CCC', mandatory: false, repetition: 1 }
+        { content: 'DDD', mandatory: false, repetition: 1 }
       ]);
     });
     it('can include only one repetition', function () {
       expect(function () { tracker.accept('AAA'); }).not.toThrow();
-      expect(function () { tracker.accept('CCC'); }).not.toThrow();
+      expect(function () { tracker.accept('DDD'); }).not.toThrow();
     });
     it('can include the maximal number of repetitions', function () {
       expect(function () { tracker.accept('AAA'); }).not.toThrow();
       expect(function () { tracker.accept('AAA'); }).not.toThrow();
       expect(function () { tracker.accept('AAA'); }).not.toThrow();
-      expect(function () { tracker.accept('CCC'); }).not.toThrow();
+      expect(function () { tracker.accept('DDD'); }).not.toThrow();
     });
     it('can skip to the next entry', function () {
-      expect(function () { tracker.accept('CCC'); }).not.toThrow();
+      expect(function () { tracker.accept('DDD'); }).not.toThrow();
     });
     it('should not exceed maximum repetitions', function () {
+      expect(function () { tracker.accept('AAA'); }).not.toThrow();
+      expect(function () { tracker.accept('BBB'); }).not.toThrow();
       expect(function () { tracker.accept('AAA'); }).not.toThrow();
       expect(function () { tracker.accept('BBB'); }).not.toThrow();
       expect(function () { tracker.accept('AAA'); }).not.toThrow();
@@ -112,5 +112,66 @@ describe('Tracker', function () {
     it('should include all mandatory elements if included', function () {
       expect(function () { tracker.accept('BBB'); }).toThrow();
     });
+  });
+  describe('expecting an optional child group', function () {
+    beforeEach(function () {
+      tracker = new Tracker([
+        { content: [
+          { content: 'AAA', mandatory: false, repetition: 1 },
+          { content: [
+            { content: 'BBB', mandatory: false, repetition: 1 }
+          ], mandatory: false, repetition: 3}
+        ], mandatory: false, repetition: 3}
+      ]);
+      // Move the current tracker position inside the top-level group.
+      expect(function () { tracker.accept('AAA'); }).not.toThrow();
+    });
+    it('should retry the parent group after skipping the child group', function () {
+      // Tests specifically to see what happens after leaving a group while the
+      // tracker is in a probing state.
+      expect(function () { tracker.accept('AAA'); }).not.toThrow();
+    });
+  });
+  it('can skip a mandatory group without mandatory elements', function () {
+    // Checking for mandatory omissions should only be done for segments and not
+    // for groups.
+    tracker = new Tracker([
+      { content: [
+        { content: 'AAA', mandatory: false, repetition: 1 },
+        { content: 'BBB', mandatory: false, repetition: 1 }
+      ], mandatory: true, repetition: 3},
+      { content: 'CCC', mandatory: false, repetition: 1 }
+    ]);
+    expect(function () { tracker.accept('CCC'); }).not.toThrow();
+  });
+  it('should return from a mandatory path when an omission is encountered', function () {
+    tracker = new Tracker([
+      { content: [
+        { content: [
+          { content: 'AAA', mandatory: true, repetition: 1 }
+        ], mandatory: true, repetition: 3},
+        { content: 'BBB', mandatory: false, repetition: 1 }
+      ], mandatory: false, repetition: 3},
+      { content: 'CCC', mandatory: false, repetition: 1 }
+    ]);
+    expect(function () { tracker.accept('BBB'); }).toThrow();
+  });
+  it('should throw when omitting a mandatory segment after probing for a mandatory group repetition', function () {
+    // Repetitions of a mandatory group are actually conditional in the sense
+    // that a message can include the mandatory group only once. The tracker is
+    // potentially in a different state after probing for such a second
+    // repetition. In this test we look at what happens if we omit a mandatory
+    // segment after putting the tracker in such a state. It could fail if the
+    // mandatory group was added as a return path for omitted mandatory
+    // segments, but not removed when leaving the group.
+    tracker = new Tracker([
+      { content: [
+        { content: 'AAA', mandatory: false, repetition: 1 }
+      ], mandatory: true, repetition: 5},
+      { content: 'BBB', mandatory: true, repetition: 1 },
+      { content: 'CCC', mandatory: false, repetition: 1 }
+    ]);
+    expect(function () { tracker.accept('AAA'); }).not.toThrow();
+    expect(function () { tracker.accept('CCC'); }).toThrow();
   });
 });
